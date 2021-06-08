747aab90f846410f444914713d238034fcf767a2
icecube-pixel <nithinbharadwajd@outlook.com>
2021-06-08 18:23:32 +0530

    Added code to understand the licences of the repos

--- get_grok_repos.py
+++ get_grok_repos.py
@@ -4,7 +4,10 @@ from typing import Dict, Tuple, List
 import os
 import argparse
 import traceback
-
+from collections import Counter
+from tenacity import retry, stop_after_attempt, wait_exponential
+from time import sleep
+import pandas as pd
 
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
     datefmt='%Y-%m-%d:%H:%M:%S',
@@ -30,26 +33,30 @@ def get_matching_code(args: Dict)->None:
     Gets the top matches of code based on pattern where grok is used and is of not YAML etc
     """
     logger.info("Inside to get top repositories function")
-
+    master_data = []
+    observed_licences = []
     try:
-        g_obj = Github(args['token'])
+        g_obj = Github(args['token'], timeout=3000) # Overriding timeout of 3000 seconds
         pattern_file_extension = '"grok" in:file extension:j2'
         lang_to_exclude = get_query_string_to_exclude()
         _query_str = f"{pattern_file_extension}{lang_to_exclude}"
         logger.info(f"Processing query {_query_str}")
-
+        sleep(10)
         results = g_obj.search_code(_query_str)
         for repo in results:
+            master_data.append(vars(repo))
+
+            observed_licences.append(repo.license)
             file_name = str(repo).split("ContentFile(path=")[1].replace('"',"")[:-1].replace("/", "_")
             path_to_dump = os.path.join(os.getcwd(), "data", file_name)
             logger.info("Dumping file {}".format(file_name))
             with open(path_to_dump, "wb") as f:
                 f.write(repo.decoded_content)
-
+        logger.info(Counter(observed_licences))
     except Exception as e:
         logger.error(e)
         logger.error(traceback.format_exc())
-
+    pd.DataFrame(master_data).to_csv("RepoData.csv", index=False)
 
 def get_inputs()->Dict:
     """Gets the username and password from the console """
